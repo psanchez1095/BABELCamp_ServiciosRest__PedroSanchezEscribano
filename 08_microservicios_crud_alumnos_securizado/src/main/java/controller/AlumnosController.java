01882ddc3e2938203afa992e364737f316336250
Ps95se <52817956+psanchez1095@users.noreply.github.com>
2022-05-06 14:50:38 +0200

    Project Microservicios Crud Alumnos Securizado 08 Added

---
+++ 08_microservicios_crud_alumnos_securizado/src/main/java/controller/AlumnosController.java
@@ -0,0 +1,56 @@
+package controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import model.Alumno;
+
+import service.AlumnosService;
+
+
+@RestController
+public class AlumnosController {
+		@Autowired
+		AlumnosService service;
+		
+		@PostMapping(value="Alumno",consumes=MediaType.APPLICATION_JSON_VALUE)
+		public void alta(@RequestBody Alumno a) {
+			service.altaAlumno(a);
+		}
+		
+		@PutMapping(value="Alumno",produces=MediaType.TEXT_PLAIN_VALUE)
+		public String actualizar(@RequestParam("idAlumno") int idAlumno,@RequestParam("curso") String curso) {
+			return String.valueOf(service.actualizarAlumno(idAlumno, curso));
+		}
+		
+		@DeleteMapping(value="Alumno/{idAlumno}",produces=MediaType.TEXT_PLAIN_VALUE)
+		public String eliminar(@PathVariable("idAlumno") int idAlumno) {
+			return String.valueOf(service.eliminarAlumno(idAlumno));
+		}
+		
+		@GetMapping(value ="Alumnos",produces=MediaType.APPLICATION_JSON_VALUE)
+		public List<Alumno> alumnos(){
+			return service.buscarAlumnos();
+		}
+		
+		@GetMapping(value ="Alumno/{idAlumno}",produces=MediaType.APPLICATION_JSON_VALUE)
+		public Alumno buscarAlumno(@PathVariable("idAlumno") int idAlumno){
+			return service.buscarAlumno(idAlumno);
+		}
+		
+		@GetMapping(value ="Cursos",produces=MediaType.APPLICATION_JSON_VALUE)
+		public List<String> cursos(){
+			return service.buscarCursos();
+		}
+		
+}
