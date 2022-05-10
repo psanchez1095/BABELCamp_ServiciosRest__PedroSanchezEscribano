fb260d0c7f86abedffd02f5173d53426933a0fb1
Ps95se <52817956+psanchez1095@users.noreply.github.com>
2022-05-10 10:51:29 +0200

    CrossOrigin Added in Controller

--- 11_microservicio_vuelos/src/main/java/controller/VuelosController.java
+++ 11_microservicio_vuelos/src/main/java/controller/VuelosController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -15,6 +16,7 @@ import service.VuelosService;
 
 
 @RestController
+@CrossOrigin("*")
 public class VuelosController {
 		@Autowired
 		VuelosService service;
--- 12_microservicio_reservas/src/main/java/controller/ReservasController.java
+++ 12_microservicio_reservas/src/main/java/controller/ReservasController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import service.ReservasService;
 
 
 @RestController
+@CrossOrigin("*")
 public class ReservasController {
 		@Autowired
 		ReservasService service;
