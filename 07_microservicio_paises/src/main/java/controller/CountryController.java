77a92996ee425f01bfdaf02570177838e10af2ff
Ps95se <52817956+psanchez1095@users.noreply.github.com>
2022-05-11 13:07:12 +0200

    Project Microservicio Paises 07 -> Documentation Swagger Added

--- 07_microservicio_paises/src/main/java/controller/CountryController.java
+++ 07_microservicio_paises/src/main/java/controller/CountryController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import model.Country;
 import service.CountryService;
 
@@ -19,17 +21,21 @@ import service.CountryService;
 public class CountryController {
 	
 	@Autowired CountryService service;
-	
+	@ApiOperation(value="Devuelve una lista de nombres con los diferentes continentes ")
 	@GetMapping(value ="Regions",produces=MediaType.APPLICATION_JSON_VALUE)
 	public List<String> continentes(){
 		return service.continentes();
 	}
+	@ApiOperation(value="Devuelve una lista de paises en el continente ")
 	@GetMapping(value ="Countries/{continente}",produces=MediaType.APPLICATION_JSON_VALUE)
-	public List<Country> paiseEnContinente(@PathVariable("continente") String continente){
+	public List<Country> paiseEnContinente(@ApiParam(value="Nombre del Continente : String")
+	@PathVariable("continente") String continente){
 		return service.paisePorContinente(continente);
 	}
+	@ApiOperation(value="Devuelve un entero con el numero de habitantes en el continente ")
 	@GetMapping(value ="CountriesPopulation/{continente}",produces=MediaType.TEXT_PLAIN_VALUE)
-	public String habitantesContinente(@PathVariable("continente") String continente){
+	public String habitantesContinente(@ApiParam(value="Nombre del Continente : String")
+	@PathVariable("continente") String continente){
 		return "Habitantes " + continente +" "+Long.toString(service.habitantesPorContinente(continente)) ;
 	}
 	
--- 07_microservicio_paises/src/main/java/init/Application.java
+++ 07_microservicio_paises/src/main/java/init/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication(scanBasePackages = {"service","controller"})
+@SpringBootApplication(scanBasePackages = {"service","controller","init"})
 public class Application {
 
 	public static void main(String[] args) {
