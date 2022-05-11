dd21641cf8a8e01b7e1dada3bdee0fa05df283c0
Ps95se <52817956+psanchez1095@users.noreply.github.com>
2022-05-11 10:57:58 +0200

    Project Microservicio Estudiantes Cliente de Alumnos Por Eureka 16 Added

---
+++ 16_microservicio_estudiantes_cliente_de_alumnos_por_eureka/src/main/java/service/StudentServiceImpl.java
@@ -0,0 +1,38 @@
+package service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import model.Student;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+	RestTemplate template;
+	String urlBase="http://servicio-alumnos/academia";
+	
+	public StudentServiceImpl(@Autowired RestTemplate template) {
+		super();
+		this.template = template;
+	}
+
+	@Override
+	public void altaStudent(Student s) {
+		template.postForLocation(urlBase+"/Alumno", s);
+		
+	}
+
+	@Override
+	public List<Student> studentPorNotaMinima(double puntuacion) {
+		Student[] students = template.getForObject(urlBase+"/Alumnos", Student[].class);
+		return Arrays
+		.stream(students)
+		.filter(x -> x.getPuntuacion() >= puntuacion)
+		.collect(Collectors.toList());
+	}
+
+}
