3f147ee73f60208fc16b8f151cf0fb6c427b5d1f
Ps95se <52817956+psanchez1095@users.noreply.github.com>
2022-05-16 21:08:40 +0200

    Project Microservicios Hotel Eureka 27 Added

---
+++ 27_microservicio_hotel_eureka_config/src/main/java/controller/HotelesController.java
@@ -0,0 +1,38 @@
+package controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import model.Hotel;
+import service.HotelesService;
+
+
+@RestController
+@CrossOrigin("*")
+public class HotelesController {
+		@Autowired
+		HotelesService service;
+		
+		@GetMapping(value ="HotelById/{id}",produces=MediaType.APPLICATION_JSON_VALUE)
+		public Hotel buscarHotel(@PathVariable("id") int id){
+			return service.buscarHotel(id);
+		}
+	
+		@GetMapping(value ="Hotel/{nombre}",produces=MediaType.APPLICATION_JSON_VALUE)
+		public Hotel buscarHotel(@PathVariable("nombre") String nombre){
+			return service.buscarHotel(nombre);
+		}
+		
+		@GetMapping(value ="HotelesDisponibles",produces=MediaType.APPLICATION_JSON_VALUE)
+		public List<Hotel> hoteles(){
+			return service.buscarHotelesDisponibles();
+		}
+		
+}
